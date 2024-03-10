cd90995d7673ded4ee2bc87d998ac2691846043d
amitmishra135 <mishraamit9129@gmail.com>
2024-03-10 16:12:51 +0530

    my first api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,5 +7,24 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
+@app.route("/hello_world1")
+def hello_world1():
+    return "<h1>Hello, World!1</h1>"
+
+@app.route("/test")
+def test():
+    a=10+20
+    return "this is my print function {}".format(a)
+
+@app.route("/test2/test2")
+def test2():
+    #for taking argument at the client side import request library
+    data = request.args.get('x')
+    return "this is my print function {}".format(data)
+
+
+@app.route("/hello_world2")
+def hello_world2():
+    return "<h1>Hello, World!2</h1>" 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
